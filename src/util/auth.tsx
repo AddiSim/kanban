5d532e7d722f96e009df43749deb7681385d985f
AddiSim <addisim12@gmail.com>
2024-04-22 13:51:44 +0000

    fiiiiix

--- src/util/auth.tsx
+++ src/util/auth.tsx
@@ -1,18 +1,16 @@
-import { NextResponse } from "next/server"
-import useFetch from "react-fetch-hook";
+import { NextResponse } from "next/server";
 
 export async function auth(token: string, path = '/api/authenticate') {
-	const deployedUrl = process.env.VERCEL_URL || 'http://localhost:3000';
-	const response = await fetch(deployedUrl + '/api/authenticate',
-		{
-			method: 'POST',
-			headers: { 'Content-Type': 'application/json' },
-			body: JSON.stringify({ token })
-		}
-	)
+	const apiUrl = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000';
+	const response = await fetch(`${apiUrl}${path}`, {
+		method: 'POST',
+		headers: { 'Content-Type': 'application/json' },
+		body: JSON.stringify({ token })
+	});
+
 	if (response.ok) {
-		const data = await response.json() as { login?: boolean, isAdmin?: boolean, message?: string }
-		return data
+		const data = await response.json() as { login?: boolean, isAdmin?: boolean, message?: string };
+		return data;
 	} else {
 		throw new Error(`HTTP error! status: ${response.status}`);
 	}
